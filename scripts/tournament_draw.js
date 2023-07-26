7d3be1df3b35d387ef8b1d117f60b2e15837e974
Nick-Guh <126421361+Nick-Guh@users.noreply.github.com>
2023-07-26 19:20:24 +0200

    Update tournament_draw.js

--- scripts/tournament_draw.js
+++ scripts/tournament_draw.js
@@ -1,4 +1,29 @@
-import { randomizeArrayOrder, cloneArray } from "scripts/utils.js";
+//import { randomizeArrayOrder, cloneArray } from "scripts/utils.js";
+function randomizeArrayOrder(array){
+    let randomizedArray = [];
+    let arrayClone = cloneArray(array);
+  
+    for (let i = 0; i < array.length; i++) {
+      let randomIndex = Math.floor(Math.random()*(arrayClone.length-1));
+      randomizedArray[i] = arrayClone[randomIndex];
+      arrayClone.splice(randomIndex, 1);
+    }
+  
+    return randomizedArray
+  }      
+  
+  function cloneArray(array) {
+    let arrayClone = [];
+  
+    for (let i=0; i < array.length; i++) {
+      arrayClone[i] = array[i];
+    };
+  
+    return arrayClone;
+  }
+
+
+
 //creates the array in which the teams have to be stored
 let partecipants = [];
 for (let i = 0; i < 40; i++) {
